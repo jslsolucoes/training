0473c8ca5a5fb12528ec58463b5626ac9c0c3114
Jonatan Lemes <jonatan@jslsolucoes.com>
2020-06-23 01:27:50 -0300

    add skeleton

---
+++ skeleton/src/test/java/com/jslsolucoes/skeleton/SkeletonTest.java
@@ -0,0 +1,18 @@
+package com.jslsolucoes.skeleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+public class SkeletonTest {
+
+    @Test
+    public void assertTrue() {
+	Assertions.assertTrue(true);
+    }
+}
